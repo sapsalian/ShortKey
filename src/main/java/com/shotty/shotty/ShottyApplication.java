af203092393777569e469f83183e2ce6290dd78e
Yongjin An <yongjin5352@gmail.com>
2024-07-24 16:22:00 +0900

    feat: initiate project

---
+++ src/main/java/com/shotty/shotty/ShottyApplication.java
@@ -0,0 +1,13 @@
+package com.shotty.shotty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShottyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShottyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shotty/shotty/ShottyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotty.shotty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShottyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
