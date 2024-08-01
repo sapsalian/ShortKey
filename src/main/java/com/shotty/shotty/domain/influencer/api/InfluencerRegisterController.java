46100b3bcb2c0379f8894b5bd3d82bc0f2139bbb
peanut <peanut1998@naver.com>
2024-08-01 11:51:28 +0900

    fix: registerInfluencer에 @TokenId 적용

--- src/main/java/com/shotty/shotty/domain/influencer/api/InfluencerRegisterController.java
+++ src/main/java/com/shotty/shotty/domain/influencer/api/InfluencerRegisterController.java
@@ -4,23 +4,23 @@ import com.shotty.shotty.domain.influencer.application.InfluencerService;
 import com.shotty.shotty.domain.influencer.domain.Influencer;
 import com.shotty.shotty.domain.influencer.dto.RegisterInfluencerDto;
 import com.shotty.shotty.domain.influencer.dto.SaveInfluencerDto;
+import com.shotty.shotty.global.common.custom_annotation.annotation.TokenId;
 import com.shotty.shotty.global.common.dto.ResponseDto;
 import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class InfluencerRegisterController {
     private final InfluencerService influencerService;
-    @PostMapping("/api/influencers")
-    public ResponseEntity<ResponseDto<Null>> registerInfluencer(@RequestBody RegisterInfluencerDto registerInfluencerDto) {
-        //user_id추출
-        Long user_id = 2L;
-
+    @PostMapping("/influencers")
+    public ResponseEntity<ResponseDto<Null>> registerInfluencer(@TokenId Long user_id, @RequestBody RegisterInfluencerDto registerInfluencerDto) {
         SaveInfluencerDto saveInfluencerDto = SaveInfluencerDto.from(registerInfluencerDto);
         influencerService.register(user_id, saveInfluencerDto);
         ResponseDto<Null> responseDto = new ResponseDto<>(
