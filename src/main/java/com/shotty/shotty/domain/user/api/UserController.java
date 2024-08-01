5d4e8dba6825e1dfebf2ab15beff4e4a10358ee1
Yongjin An <yongjin5352@gmail.com>
2024-08-01 12:24:49 +0900

    feat: 현재 사용자 조회 api에 swagger 설정 적용.
    token을 사용하기 위한 설정이 추가로 필요합니다.

--- src/main/java/com/shotty/shotty/domain/user/api/UserController.java
+++ src/main/java/com/shotty/shotty/domain/user/api/UserController.java
@@ -5,6 +5,9 @@ import com.shotty.shotty.domain.user.domain.User;
 import com.shotty.shotty.domain.user.dto.UserResponseDto;
 import com.shotty.shotty.global.common.custom_annotation.annotation.TokenId;
 import com.shotty.shotty.global.common.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name= "사용자 정보 API 처리 컨트롤러",description = "사용자 정보 조회, 수정, 삭제를 위한 API를 처리하는 컨트롤러")
 public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/me")
+    @Operation(summary = "현재 접속중인 사용자 정보 조회", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<ResponseDto<UserResponseDto>> getCurrentUser(@TokenId Long id) {
         UserResponseDto userResponseDto = userService.findById(id);
 
