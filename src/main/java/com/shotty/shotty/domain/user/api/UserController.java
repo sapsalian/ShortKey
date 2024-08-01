d83caa17f24f91b2d454bfc71aaed687883fab3e
Yongjin An <yongjin5352@gmail.com>
2024-08-01 14:49:27 +0900

    feat: 현재 로그인된 사용자 정보를 수정하는 api를 담당하는 컨트롤러 메서드를 구현하였습니다.

--- src/main/java/com/shotty/shotty/domain/user/api/UserController.java
+++ src/main/java/com/shotty/shotty/domain/user/api/UserController.java
@@ -2,6 +2,7 @@ package com.shotty.shotty.domain.user.api;
 
 import com.shotty.shotty.domain.user.application.UserService;
 import com.shotty.shotty.domain.user.domain.User;
+import com.shotty.shotty.domain.user.dto.UserPatchDto;
 import com.shotty.shotty.domain.user.dto.UserResponseDto;
 import com.shotty.shotty.global.common.custom_annotation.annotation.TokenId;
 import com.shotty.shotty.global.common.dto.ResponseDto;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +36,7 @@ public class UserController {
         return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping("api/users/{id}")
+    @GetMapping("/api/users/{id}")
     @Operation(summary = "id를 이용해 사용자 정보 조회")
     public ResponseEntity<ResponseDto<UserResponseDto>> getUserById(@PathVariable Long id) {
         UserResponseDto userResponseDto = userService.findById(id);
@@ -42,6 +44,20 @@ public class UserController {
         ResponseDto<UserResponseDto> responseDto = new ResponseDto<>(
                 2002,
                 "사용자 정보 조회 성공",
+                userResponseDto
+        );
+
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PatchMapping("/api/users")
+    @Operation(summary = "현재 로그인된 사용자 정보 수정")
+    public ResponseEntity<ResponseDto<UserResponseDto>> editUser(@TokenId Long id, UserPatchDto userPatchDto) {
+        UserResponseDto userResponseDto = userService.patch(id, userPatchDto);
+
+        ResponseDto<UserResponseDto> responseDto = new ResponseDto<>(
+                2003,
+                "사용자 정보 수정 성공",
                 userResponseDto
         );
 
