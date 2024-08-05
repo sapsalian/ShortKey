2e889819d27451aa6f71dc42cf95994fcf08865b
Yongjin An <yongjin5352@gmail.com>
2024-08-05 11:44:33 +0900

    feat: Cors 허용 Origin에 프론트 배포 주소 추가

--- src/main/java/com/shotty/shotty/ShottyApplication.java
+++ src/main/java/com/shotty/shotty/ShottyApplication.java
@@ -18,7 +18,7 @@ public class ShottyApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173", "https://shotty-one.vercel.app");
 			}
 		};
 	}
