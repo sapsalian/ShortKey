30017ff3b5f7f6cde94174fdcb1ef9dff78fcf42
Yongjin An <yongjin5352@gmail.com>
2024-08-05 14:46:55 +0900

    feat: 쿼리 파라미터로 요청받은 sort 값에 해당하는 필드가 존재하지 않을 시 발생할 커스텀 예외 정의

---
+++ src/main/java/com/shotty/shotty/global/common/exception/NoSuchSortFieldException.java
@@ -0,0 +1,19 @@
+package com.shotty.shotty.global.common.exception;
+
+public class NoSuchSortFieldException extends RuntimeException {
+    public NoSuchSortFieldException() {
+        super("요청하신 sort 쿼리 파라미터 값과 일치하는 필드가 존재하지 않습니다.");
+    }
+
+    public NoSuchSortFieldException(String message) {
+        super(message);
+    }
+
+    public NoSuchSortFieldException(Throwable cause) {
+        super(cause);
+    }
+
+    public NoSuchSortFieldException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
