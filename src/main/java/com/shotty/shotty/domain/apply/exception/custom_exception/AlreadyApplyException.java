32af662bd9844b13ab9d8020f82fa9cb8198f408
peanut <peanut1998@naver.com>
2024-08-06 16:48:53 +0900

    fix : exception 수정

--- src/main/java/com/shotty/shotty/domain/apply/exception/custom_exception/AlreadyApplyException.java
+++ src/main/java/com/shotty/shotty/domain/apply/exception/custom_exception/AlreadyApplyException.java
@@ -2,6 +2,8 @@ package com.shotty.shotty.domain.apply.exception.custom_exception;
 
 public class AlreadyApplyException extends RuntimeException {
     public AlreadyApplyException() {}
-    public AlreadyApplyException(String message) {}
+    public AlreadyApplyException(String message) {
+        super(message);
+    }
 
 }
