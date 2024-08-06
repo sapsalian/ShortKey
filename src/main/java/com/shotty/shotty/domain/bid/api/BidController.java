83486d19ead271ecbad42fe37c860ec0fbf1e64f
Yongjin An <yongjin5352@gmail.com>
2024-08-06 17:06:16 +0900

    feat: 지원 입찰을 요청한 사용자가 공고 작성자인지 확인하는 로직 적용

--- src/main/java/com/shotty/shotty/domain/bid/api/BidController.java
+++ src/main/java/com/shotty/shotty/domain/bid/api/BidController.java
@@ -3,12 +3,13 @@ package com.shotty.shotty.domain.bid.api;
 import com.shotty.shotty.domain.bid.application.BidService;
 import com.shotty.shotty.domain.bid.dto.BidRequestDto;
 import com.shotty.shotty.domain.bid.dto.BidResponseDto;
+import com.shotty.shotty.global.common.custom_annotation.annotation.TokenId;
 import com.shotty.shotty.global.common.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +18,16 @@ public class BidController {
     private final BidService bidService;
 
     @PostMapping("applies/{applyId}/bids")
-    public ResponseEntity<ResponseDto<BidResponseDto>> doBid(@PathVariable Long applyId) {
-        BidResponseDto bidResponseDto = bidService.create(new BidRequestDto(applyId));
+    public ResponseEntity<ResponseDto<BidResponseDto>> doBid(
+            @Parameter(hidden = true)
+            @TokenId
+            Long userId,
+
+            @PathVariable
+            Long applyId
+    ) {
+        BidRequestDto bidRequestDto = new BidRequestDto(applyId, userId);
+        BidResponseDto bidResponseDto = bidService.create(bidRequestDto);
 
         ResponseDto<BidResponseDto> responseDto = new ResponseDto<>(
                 2011,
--- src/main/java/com/shotty/shotty/domain/bid/dto/BidRequestDto.java
+++ src/main/java/com/shotty/shotty/domain/bid/dto/BidRequestDto.java
@@ -4,8 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record BidRequestDto(
-        @Schema(description = "지원 내역에 대한 식별키", example = "1")
-        @NotNull(message = "입찰을 위해서는 지원 내역에 대한 식별키가 반드시 필요합니다.")
-        Long applyId
+        Long applyId,
+
+        Long userId
 ) {
 }
