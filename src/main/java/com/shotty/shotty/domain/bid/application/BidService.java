b8d9ab93bc3e1d54d978b48fb68d6947fdcf9270
Yongjin An <yongjin5352@gmail.com>
2024-08-06 16:30:21 +0900

    feat: 입찰 등록을 위한 서비스 메서드 구현

---
+++ src/main/java/com/shotty/shotty/domain/bid/application/BidService.java
@@ -0,0 +1,29 @@
+package com.shotty.shotty.domain.bid.application;
+
+import com.shotty.shotty.domain.apply.dao.ApplyRepository;
+import com.shotty.shotty.domain.apply.domain.Apply;
+import com.shotty.shotty.domain.bid.dao.BidRepository;
+import com.shotty.shotty.domain.bid.domain.Bid;
+import com.shotty.shotty.domain.bid.dto.BidRequestDto;
+import com.shotty.shotty.domain.bid.dto.BidResponseDto;
+import com.shotty.shotty.global.common.exception.custom_exception.NoSuchResourcException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BidService {
+    private final BidRepository bidRepository;
+    private final ApplyRepository applyRepository;
+
+    public BidResponseDto create(BidRequestDto requestDto) {
+        Long applyId = requestDto.applyId();
+        Apply apply = applyRepository.findById(applyId)
+                .orElseThrow(() -> new NoSuchResourcException("존재하지 않는 지원 내역에 대한 입찰입니다."));
+
+
+        Bid bid = bidRepository.save(new Bid(apply));
+
+        return BidResponseDto.from(bid);
+    }
+}
