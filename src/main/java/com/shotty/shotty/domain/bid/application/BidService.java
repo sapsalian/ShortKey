7db1f01f8ce5bd2358bd84678a34b63468e7374b
Yongjin An <yongjin5352@gmail.com>
2024-08-06 17:06:16 +0900

    feat: 지원 입찰을 요청한 사용자가 공고 작성자인지 확인하는 로직 적용

--- src/main/java/com/shotty/shotty/domain/bid/application/BidService.java
+++ src/main/java/com/shotty/shotty/domain/bid/application/BidService.java
@@ -7,6 +7,7 @@ import com.shotty.shotty.domain.bid.domain.Bid;
 import com.shotty.shotty.domain.bid.dto.BidRequestDto;
 import com.shotty.shotty.domain.bid.dto.BidResponseDto;
 import com.shotty.shotty.global.common.exception.custom_exception.NoSuchResourcException;
+import com.shotty.shotty.global.common.exception.custom_exception.PermissionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,12 @@ public class BidService {
         Apply apply = applyRepository.findById(applyId)
                 .orElseThrow(() -> new NoSuchResourcException("존재하지 않는 지원 내역에 대한 입찰입니다."));
 
+        Long postAuthorId = apply.getPost().getAuthor().getId();
+        Long requesterId = requestDto.userId();
+
+        if (!postAuthorId.equals(requesterId)) {
+            throw new PermissionException("입찰은 공고 작성자만 가능합니다.");
+        }
 
         Bid bid = bidRepository.save(new Bid(apply));
 
