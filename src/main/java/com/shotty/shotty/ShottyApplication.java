c4221f995822336f9457d329e0cd782a6eb7a27d
Yongjin An <yongjin5352@gmail.com>
2024-08-06 11:44:34 +0900

    fix: 허용하는 Methods에 PATCH 추가

--- src/main/java/com/shotty/shotty/ShottyApplication.java
+++ src/main/java/com/shotty/shotty/ShottyApplication.java
@@ -20,7 +20,7 @@ public class ShottyApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
 						.allowedOrigins("http://localhost:5173", "https://shotty-one.vercel.app")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
 			}
