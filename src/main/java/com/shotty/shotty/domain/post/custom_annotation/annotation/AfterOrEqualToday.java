9dbceb64bec19fe1651bea0d6ecc7f3edf317653
Yongjin An <yongjin5352@gmail.com>
2024-08-07 12:13:25 +0900

    fix: 실행중에 검증이 실행될 수 있도록 Retention Policy 수정

--- src/main/java/com/shotty/shotty/domain/post/custom_annotation/annotation/AfterOrEqualToday.java
+++ src/main/java/com/shotty/shotty/domain/post/custom_annotation/annotation/AfterOrEqualToday.java
@@ -5,10 +5,13 @@ import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Constraint(validatedBy = AfterOrEqualTodayValidator.class)
 @Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface AfterOrEqualToday {
     String message() default "현재 날짜보다 이후여야 합니다.";
 
