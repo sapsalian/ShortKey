82fadf8877704098cc594f37ef7971aa14122eba
Yongjin An <yongjin5352@gmail.com>
2024-08-09 21:32:28 +0900

    fix: 데이터가 수정되는 Delete, Update 쿼리에 @Transactional, @Modifying 추가

--- src/main/java/com/shotty/shotty/domain/bid/dao/BidRepository.java
+++ src/main/java/com/shotty/shotty/domain/bid/dao/BidRepository.java
@@ -1,7 +1,9 @@
 package com.shotty.shotty.domain.bid.dao;
 
 import com.shotty.shotty.domain.bid.domain.Bid;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,13 +14,15 @@ public interface BidRepository extends JpaRepository<Bid, Long> {
     boolean existsByApplyId(Long applyId);
     void deleteByApplyId(Long applyId);
 
-    @Query("DELETE FROM Bid bid WHERE bid.id IN (" +
-                    "SELECT bid.id " +
-                    "FROM Bid bid " +
-                    "INNER JOIN Apply apply ON bid.apply.id = apply.id " +
-                    "INNER JOIN Influencer influencer ON apply.influencer.id = influencer.id " +
-                    "WHERE :influencerId = influencer.id" +
-                    ")"
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM bids WHERE id IN (" +
+                "SELECT bids.id FROM bids " +
+                "INNER JOIN applies ON bids.apply_id = applies.id " +
+                "INNER JOIN influencers ON applies.influencer_id = influencers.influencer_id " +
+                "WHERE influencers.influencer_id = :influencerId)",
+
+                nativeQuery = true
     )
     void deleteAllByInfluencerId(@Param("influencerId") Long influencerId);
 }
--- src/main/java/com/shotty/shotty/domain/post/dao/PostRepository.java
+++ src/main/java/com/shotty/shotty/domain/post/dao/PostRepository.java
@@ -1,9 +1,11 @@
 package com.shotty.shotty.domain.post.dao;
 
 import com.shotty.shotty.domain.post.domain.Post;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,17 +13,22 @@ import org.springframework.stereotype.Repository;
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAll(Pageable pageable);
 
-    @Query(
-            "UPDATE Post post " +
-                    "SET post.author.id = :newUserId " +
-                    "WHERE post.author.id = :oldUserId"
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE posts " +
+                    "SET posts.author_id = :newUserId " +
+                    "WHERE posts.author_id = :oldUserId",
+            nativeQuery = true
     )
     void updateAllByUserId(Long oldUserId, Long newUserId);
 
+    @Transactional
+    @Modifying
     @Query(
-            "UPDATE Post post " +
-                    "SET post.active = false " +
-                    "WHERE post.author.id = :userId"
+            value = "UPDATE posts " +
+                    "SET posts.active = false " +
+                    "WHERE posts.author_id = :userId",
+            nativeQuery = true
     )
     void deactivateAllByUserId(Long userId);
 }
