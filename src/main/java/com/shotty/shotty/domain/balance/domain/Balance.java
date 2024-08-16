f851f8fedbdfc9d8bf22a6f43958dfc615b3a4fb
Yongjin An <yongjin5352@gmail.com>
2024-08-16 12:49:12 +0900

    feat: 잔고 엔티티 정의

---
+++ src/main/java/com/shotty/shotty/domain/balance/domain/Balance.java
@@ -0,0 +1,32 @@
+package com.shotty.shotty.domain.balance.domain;
+
+import com.shotty.shotty.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "balances")
+@Getter
+public class Balance {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long totalAmount;
+
+    private String account;
+
+    private String bank;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public void deposit(Long amount) {
+        totalAmount += amount;
+    }
+
+    public void withdraw(Long amount) {
+        totalAmount -= amount;
+    }
+}
