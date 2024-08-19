8d998c92ffabb9400fc5bee0dba22225aa25b3ad
Yongjin An <yongjin5352@gmail.com>
2024-08-19 11:04:32 +0900

    feat: cors url 추가

--- src/main/java/com/shotty/shotty/ShottyApplication.java
+++ src/main/java/com/shotty/shotty/ShottyApplication.java
@@ -19,7 +19,7 @@ public class ShottyApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173", "https://shotty-one.vercel.app")
+						.allowedOrigins("http://localhost:5173", "https://shotty-one.vercel.app", "https://shotty-app.shop")
 						.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
