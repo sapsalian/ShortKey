8a1ab6be506b43d20c2a870ce39357e56b7bdc4b
peanut <peanut1998@naver.com>
2024-08-19 13:50:07 +0900

    feat: 필터에서 토큰 여부 확인 및 검증에 대한 http 응답 설정

--- src/main/java/com/shotty/shotty/global/auth/filter/LoginCheckFilter.java
+++ src/main/java/com/shotty/shotty/global/auth/filter/LoginCheckFilter.java
@@ -1,10 +1,13 @@
 package com.shotty.shotty.global.auth.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shotty.shotty.global.common.dto.ResponseDto;
 import com.shotty.shotty.global.util.JwtProvider;
 import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,8 +20,10 @@ import java.util.List;
 public class LoginCheckFilter implements Filter {
     private final JwtProvider jwtProvider;
     private static final List<String> whiteList = Arrays.asList(
-            //"/api/auth","/swagger-ui","/v3/api-docs"
-            "/"
+            "/api/auth/register","/api/auth/login",
+            "/api/influencers",
+            "/swagger-ui","/v3/api-docs"
+            //"/"
     );
 
     @Override
@@ -35,23 +40,22 @@ public class LoginCheckFilter implements Filter {
         }
         //토큰 여부 확인
         String accessToken = getAccessToken(httpRequest);
-        log.info("Access token: {},path= {}", accessToken,path);
+        
+        log.info("토큰 여부 확인: Access token: {},path= {}", accessToken,path);
         if(accessToken == null ) {
-            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            httpResponse.getWriter().write("Missing header");
+            setResponse(httpResponse,HttpServletResponse.SC_UNAUTHORIZED,4010,"missing header");
             return;
         }
 
         //accessToken 검증
+        log.info("accessToken 검증");
         try {
             jwtProvider.validateToken(accessToken);
         }catch (ExpiredJwtException  e) {
-            httpResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            httpResponse.getWriter().write("Expired accessToken");
+            setResponse(httpResponse,HttpServletResponse.SC_BAD_REQUEST,4002,"Expired accessToken");
             return;
         }catch (Exception e) {
-            httpResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            httpResponse.getWriter().write("Invalid accessToken");
+            setResponse(httpResponse,HttpServletResponse.SC_BAD_REQUEST,4003,"Invalid access token");
             return;
         }
 
@@ -59,6 +63,20 @@ public class LoginCheckFilter implements Filter {
 
 
         filterChain.doFilter(request, response);
+    }
+
+    private void setResponse(HttpServletResponse httpResponse,int httpStatus,int code,String statusMsg) throws IOException {
+        httpResponse.setStatus(httpStatus);
+        httpResponse.setContentType("application/json");
+        httpResponse.setCharacterEncoding("utf-8");
+        ResponseDto<Null> responseDto = new ResponseDto<>(
+                code,
+                statusMsg,
+                null
+        );
+        ObjectMapper objectMapper = new ObjectMapper();
+        String responseJson = objectMapper.writeValueAsString(responseDto);
+        httpResponse.getWriter().write(responseJson);
     }
 
     private String getAccessToken(HttpServletRequest request) {
