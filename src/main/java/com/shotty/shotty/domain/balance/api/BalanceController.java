ba3bcba788089e9665ff49ea4b223a5f81f71633
Yongjin An <yongjin5352@gmail.com>
2024-08-20 13:45:34 +0900

    feat : 입금 API 구현

--- src/main/java/com/shotty/shotty/domain/balance/api/BalanceController.java
+++ src/main/java/com/shotty/shotty/domain/balance/api/BalanceController.java
@@ -2,12 +2,15 @@ package com.shotty.shotty.domain.balance.api;
 
 import com.shotty.shotty.domain.balance.application.BalanceService;
 import com.shotty.shotty.domain.balance.dto.BalanceResDto;
+import com.shotty.shotty.domain.balance.dto.ChangeBalanceDto;
 import com.shotty.shotty.global.common.custom_annotation.annotation.TokenId;
 import com.shotty.shotty.global.common.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +26,20 @@ public class BalanceController {
         ResponseDto<BalanceResDto> responseDto = new ResponseDto<>(
                 2002,
                 "잔액 조회 성공",
+                balanceResDto
+        );
+
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/api/balance/deposit")
+    @Operation(summary = "입금")
+    public ResponseEntity<ResponseDto<BalanceResDto>> deposit(@TokenId Long userId, @RequestBody ChangeBalanceDto changeBalanceDto) {
+        BalanceResDto balanceResDto = balanceService.deposit(userId, changeBalanceDto);
+
+        ResponseDto<BalanceResDto> responseDto = new ResponseDto<>(
+                2007,
+                "입금 완료",
                 balanceResDto
         );
 
--- src/main/java/com/shotty/shotty/domain/balance/application/BalanceService.java
+++ src/main/java/com/shotty/shotty/domain/balance/application/BalanceService.java
@@ -1,6 +1,7 @@
 package com.shotty.shotty.domain.balance.application;
 
 import com.shotty.shotty.domain.balance.dto.BalanceResDto;
+import com.shotty.shotty.domain.balance.dto.ChangeBalanceDto;
 import com.shotty.shotty.domain.user.dao.UserRepository;
 import com.shotty.shotty.domain.user.domain.User;
 import com.shotty.shotty.domain.user.exception.custom_exception.UserNotFoundException;
@@ -14,7 +15,18 @@ public class BalanceService {
 
     public BalanceResDto getBalanceByUserId(Long userId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException("사용자가 존재하지 않습니다."));
+                .orElseThrow(() -> new UserNotFoundException("존재하지 않는 사용자입니다."));
+
+        return BalanceResDto.from(user);
+    }
+
+    public BalanceResDto deposit(Long userId, ChangeBalanceDto changeBalanceDto) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("존재하지 않는 사용자입니다."));
+
+
+        user.deposit(changeBalanceDto.amount());
+        user = userRepository.save(user);
 
         return BalanceResDto.from(user);
     }
