f1ca1909d339cda5f149f45c86f2290f9ce090db
peanut <peanut1998@naver.com>
2024-08-28 14:11:12 +0900

    feat: payment 입출금 기능 추가

--- src/main/java/com/shotty/shotty/domain/payment/dao/PaymentRepository.java
+++ src/main/java/com/shotty/shotty/domain/payment/dao/PaymentRepository.java
@@ -1,9 +1,17 @@
 package com.shotty.shotty.domain.payment.dao;
 
 import com.shotty.shotty.domain.payment.domain.Payment;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Payment p WHERE p.bid.apply.influencer.id = :influencerId")
+    void deleteAllByInfluencerId(@Param("influencerId") Long influencerId);
 }
